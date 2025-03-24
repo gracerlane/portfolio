1aea3248167a1077be17a58dd8468c1379c31bec
anysketch <glane0900@gmail.com>
2025-03-24 13:06:02 -0700

    Update about section title gradient

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -30,13 +30,13 @@ export const About = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="max-w-3xl mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-pink-400 to-pink-111 bg-clip-text text-transparent text-center">
-            {" "}
-            About Me
-          </h2>
-
-          <div className="rounded-xl p-8 border-black/20 border hover:-translate-y-1 transition-all">
+				
+        <div className="max-w-5xl mx-auto px-4">
+					<h2 className="mx-auto w-40 text-3xl font-bold mb-8 bg-gradient-to-r from-pink-400 to-pink-200 bg-clip-text text-transparent text-center">
+						{" "}
+						About Me
+					</h2>
+          <div className="rounded-xl p-8 border-black/20 border">
             <p className="text-gray-300 mb-6">
               
             </p>
