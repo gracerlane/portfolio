c7ac32e2fa8aca8d31b57261ebf4334579fb673a
anysketch <glane0900@gmail.com>
2025-05-14 17:57:37 -0700

    update contact form submission

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { RevealOnScroll } from "../RevealOnScroll";
-// import emailjs from "emailjs-com";
-import emailjs from "@emailjs/browser";
+import emailjs from "emailjs-com";
+// import emailjs from "@emailjs/browser";
 
 export const Contact = () => {
   const [formData, setFormData] = useState({
